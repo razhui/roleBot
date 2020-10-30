4f0b99347b9ebedf9557accde0702441127f0af4
Razhui <prevgu@gmail.com>
2020-10-29 21:19:50 -0700

    add role assign function

--- index.js
+++ index.js
@@ -2,43 +2,57 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
 
-const embed = new Discord.MessageEmbed()
-    .setTitle("Select a Role")
-    .setDescription("PEKO")
-    .setColor("#dcd6f7")
+////////////////START CLIENT/////////////////
 
 client.once("ready", () => {
     console.log("Ready");
 });
 
-client.once("ready", async () => {
-    const channel = client.channels.cache.get(config.channel_id);
-    try {
-        const webhooks = await channel.fetchWebhooks();
-        const webhook = webhooks.first();
+////////////////WEBHOOK CODE/////////////////
 
-        await webhook.send(null, {
-            // avatarURL: "https://cdn.discordapp.com/attachments/342550444947800065/770840177308073994/missionDesk.png",
-            embeds: [embed],
-        });
-    } catch (error) {
-        console.error("Error trying to send: ", error);
-    }
-});
+// const embed = new Discord.MessageEmbed()
+//     .setTitle("Select a Role")
+//     .setDescription("PEKO")
+//     .setColor("#dcd6f7")
 
+// client.once("ready", async () => {
+//     const channel = client.channels.cache.get(config.channel_id);
+//     try {
+//         const webhooks = await channel.fetchWebhooks();
+//         const webhook = webhooks.first();
 
+//         await webhook.send(null, {
+//             // avatarURL: "https://cdn.discordapp.com/attachments/342550444947800065/770840177308073994/missionDesk.png",
+//             embeds: [embed],
+//         });
+//     } catch (error) {
+//         console.error("Error trying to send: ", error);
+//     }
+// });
+
+////////////////MESSAGE CODE/////////////////
 
 client.on('message', message => {
-	if (message.content === config.prefix + "ping") {
-        message.reply("Pong.");
-    } else if (message.content === config.prefix + "server"){
-        message.reply("This server's name is " + message.guild.name);
+    //Adds user to role
+    if (message.content.toLowerCase() === "academy student") {
+        let userInput = message.content.toLowerCase();
+        let role = message.guild.roles.cache.find(role => role.name.toLowerCase() === userInput);
+        message.member.roles.add(role)
+        message.channel.send("Welcome to the Academy");
     }
+
+    //BOT REACTS WITH YOUSORO
+    if (message.content.toLowerCase() === "yousoro") {
+        const reactYousoro = message.guild.emojis.cache.find(emoji => emoji.name === "Yousoro7")
+        message.react(reactYousoro);
+    }
+
 });
 
 client.login(config.token);
 
-//1. bot displays available roles for users to choose via emotes with webhook  
-//DONE  a. get bot to display webhook w/ some description
-//  b. get bot to display emotes with webhook
-//2. after selection of emote bot will assign user role
+//1.(DONE Bot can send webhook) Bot will display welcome webook w/ information regarding how to select a role
+//2.(DONE functionality is to listen) Bot will listen for "role" user types
+//3.(DONE) Bot will assign role that user types
+//4. Delete user message
+//  a. take user input
