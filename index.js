edd2d1615711406c3a00e80c1a0ba931b1ef1cb2
Razhui <prevgu@gmail.com>
2020-11-01 11:39:52 -0800

    add after assign role bot will delete message

--- index.js
+++ index.js
@@ -33,12 +33,21 @@ client.once("ready", () => {
 ////////////////MESSAGE CODE/////////////////
 
 client.on('message', message => {
-    //Assigns user to role
+    //Assigns user to role, shortly deletes message after
     if (message.content.toLowerCase() === "academy student") {
+
         let userInput = message.content.toLowerCase();
         let role = message.guild.roles.cache.find(role => role.name.toLowerCase() === userInput);
-        message.member.roles.add(role)
-        message.channel.send("Welcome to the Academy");
+        if(role) {
+            if(message.member.roles.cache.has(role.id)) {
+                message.reply("You are already in the academy.").then(message => {message.delete({ timeout: 5000 })})
+                message.delete({timeout: 5000});
+            } else {
+                message.member.roles.add(role)
+                message.reply("Welcome to the Academy").then(message => {message.delete({ timeout: 5000 })});
+                message.delete({timeout: 5000});
+            }
+        }
     }
 
     //BOT REACTS WITH YOUSORO
@@ -54,5 +63,8 @@ client.login(config.token);
 //1.(DONE Bot can send webhook) Bot will display welcome webook w/ information regarding how to select a role
 //2.(DONE functionality is to listen) Bot will listen for "role" user types
 //3.(DONE) Bot will assign role that user types
-//4. Delete user message
-//  a. take user input
+//4.(DONE) Delete user message
+//  a. delete bot message
+//5. Promote users using substrings, keyword = "promote"
+//  a. remove user's lower rank and update to higher rank ex: promote @user Genin
+//
